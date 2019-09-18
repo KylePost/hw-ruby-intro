88f1af6b8734d6416bdfb9c48e47cb0eacae81f7
KylePost <55200708+KylePost@users.noreply.github.com>
2019-09-17 20:56:42 -0500

    Add files via upload

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,50 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum array
+  array.inject 0, :+
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum array
+  sum array.sort.last 2
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? (array, n)
+    array.combination(2).any? { |x , y| x + y == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+    'Hello, ' + name
 end
 
+# ^ = start there,/ / is a pattern, $ matches end line
+# * match thing in front zero or infanit times
+
+
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[bcdfghjklmnpqrstvwxyz]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  /^[10]*00$/.match(s) || /^0$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :price
+  attr_accessor :isbn
+
+  def initialize(isbn, price)
+    raise ArgumentError, "You done messed up" if price <= 0 || isbn.empty?
+    @price = price
+    @isbn = isbn
+  end
+
+  def price_as_string
+    sprintf("$%2.2f", @price)
+  end
+
 end
